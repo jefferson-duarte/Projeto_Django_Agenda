2bf04dfdb4646a6e6b23294a43f7137c77dd4444
Jefferson Duarte <jeffersonsduarte@live.com>
2022-05-11 22:38:46 +0100

    Configuracao do settings(BASE_DIR)

--- AGENDA/settings.py
+++ AGENDA/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'AGENDA.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,10 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'templates/static')
-]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [BASE_DIR / 'templates/static']
+MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = 'media/'
 
 # Default primary key field type
